9d4a2e6e46f1681a492fb4733bf8f4f20915819c
yanyongyu <yanyongyu_1@126.com>
2020-08-14 17:42:11 +0800

    add log

--- nonebot_test/__init__.py
+++ nonebot_test/__init__.py
@@ -6,6 +6,7 @@
 import importlib
 
 from nonebot import get_driver
+from nonebot.log import logger
 
 
 def init():
@@ -16,3 +17,5 @@ def init():
     _module = importlib.import_module(f"nonebot_test.drivers.{driver.type}")
     register_route = getattr(_module, "register_route")
     register_route()
+    logger.info(f"Nonebot test frontend will be running at: "
+                f"http://{driver.config.host}:{driver.config.port}/test/")
